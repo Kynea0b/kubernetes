e2976745fa109325df347e44418d374229b87eb2
kokeshiM0chi <frogandfrog009@gmail.com>
2024-09-16 23:30:42 +0900

    tmp

---
+++ clean-pod.sh
@@ -0,0 +1,10 @@
+# https://qiita.com/tom_negocia/items/d1302cc3ad4657ad3466
+# https://qiita.com/reoring/items/7252556bb216e242cddf
+#kubectl delete pods {$pod_name} --grace-period=0 --force
+# kubectl get namespace
+kubectl -n lemontea get pods
+
+# 作成済みpod表示
+#kubectl get pods
+kubectl delete namespace sample
+
