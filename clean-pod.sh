a71dca2bce925da560b6facab7a4fe41cbc58282
kokeshiM0chi <frogandfrog009@gmail.com>
2024-09-17 14:51:25 +0900

    fix: add script for deleting all pods

--- clean-pod.sh
+++ clean-pod.sh
@@ -8,3 +8,14 @@ kubectl -n lemontea get pods
 #kubectl get pods
 kubectl delete namespace sample
 
+
+# https://qiita.com/oguogura/items/fee90b7b2ce78413a518
+# namespace指定して全て削除
+kubectl get pod -n lemontea |awk '/cockroachdb-*/{print $1}' |xargs kubectl delete pod -n lemontea --grace-period=0 --force
+
+#https://qiita.com/reoring/items/7252556bb216e242cddf
+# 強制削除
+# kubectl delete pods {$pod_name} --grace-period=0 --force
+# これで全部削除できた
+kubectl get pod -n lemontea |awk '/cockroach*/{print $1}' |xargs kubectl delete pod -n lemontea --grace-period=0 --force 
+
