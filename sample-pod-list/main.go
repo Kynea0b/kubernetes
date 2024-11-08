c72e0e200afbc6c64a252583d55fa5da8dbbabd7
kokeshiM0chi <frogandfrog009@gmail.com>
2024-11-08 14:48:46 +0900

    tmp

--- sample-pod-list/main.go
+++ sample-pod-list/main.go
@@ -13,7 +13,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-func main() {
+func getkubeconfig() *string {
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -21,14 +21,28 @@ func main() {
 		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
+
+	return kubeconfig
+}
+
+func newClientset(kubeconfig *string) *kubernetes.Clientset {
+
 	// use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
 		panic(err.Error())
 	}
-
 	// create the clientset
 	clientset, err := kubernetes.NewForConfig(config)
+
+	return clientset
+}
+
+func main() {
+	var kubeconfig = getkubeconfig()
+
+	// create the clientset
+	clientset := newClientset(kubeconfig)
 
 	pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
@@ -44,13 +58,4 @@ func main() {
 		log.Fatal(err)
 	}
 
-}
-
-func newClient() (kubernetes.Interface, error) {
-	kubeConfig, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
-	if err != nil {
-		return nil, err
-	}
-
-	return kubernetes.NewForConfig(kubeConfig)
 }
